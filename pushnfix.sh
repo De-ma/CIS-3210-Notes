306424f1d3dd66f0535b89c802687e67ea09613d
De-ma <dabuadas@uoguelph.ca>
2019-10-22 14:34:32 -0400

    ./pushnfix.sh

--- pushnfix.sh
+++ pushnfix.sh
@@ -4,4 +4,4 @@ cp notes.wiki_html/* .
 git add .
 git commit -m $0
 git push
-rm -v !("README.md"| "notes.wiki" | "notes.wiki_html" | "pushnfix.sh")
+rm -v !("README.md"|"notes.wiki"|"notes.wiki_html"|"pushnfix.sh")
